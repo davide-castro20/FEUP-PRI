58d1fd9876029d791321955f9eb3d41f98f700e9
davide-castro20 <davideafc@gmail.com>
2021-12-06 23:37:47 +0000

    Elastic document setup

---
+++ load_elastic_data.py
@@ -0,0 +1,23 @@
+import json
+from elasticsearch import Elasticsearch
+
+
+f1 = open("elastic_datasets/steam.json", "r")
+steam_json = json.load(f1)
+
+es = Elasticsearch()
+i = 1
+c = 1000
+data_send = []
+for doc in steam_json[:1000]:
+    data_send.append({"index" : { "_index" : "games_test", "_id" : str(i)} })
+    data_send.append(doc)
+    i = i + 1
+    if(c == 0):
+        response = es.bulk(index='games_test', body=data_send)
+        c = 1000
+        data_send = []
+    c -= 1
+
+if len(data_send) > 0:
+    response = es.bulk(index='games_test', body=data_send)
